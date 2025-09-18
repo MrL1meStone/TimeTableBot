14a66057e0880b2d75a83ca4d200515f204fa81c
sgooi <vbv0706@gmail.com>
2025-09-18 23:12:42 +0400

    Немного работы с бд

--- database.py
+++ database.py
@@ -23,6 +23,27 @@ CREATE TABLE IF NOT EXISTS Today (
 id INTEGER PRIMARY KEY,
 time TIME) ''')
 
+def check_source(source) -> True | ValueError:
+	"""
+	Checks the spelling of arguments for functions / Проверяет правильность написания аргументов функций
+	:param source: table of DB that need to check spell / Таблица БД написание которой надо проверить
+	:return: True if spell is right else ValueError / True если все верно, иначе ValueError
+	"""
+	if source not in ("Groups","Weekly","Tomorrow","Today"):
+		raise ValueError(f"Некорректное название таблицы {source} для поиска")
+	return True
+
+def add_to(source, chat_id, time) -> None:
+	"""
+	Adds chat id to DB to use timetable notifications /Добавляет ID чата в телеграмме для дальнейшего использования
+	:param source: table of DB where it's need to search /Таблица БД где нужно искать
+	:param chat_id: ID of telegram chat where bot will send timetable /ID чата в телеграмме где бот будет публиковать расписание
+	:param time: time when it's need to send timetable / Время когда надо присылать расписание
+	:return: None
+	"""
+	check_source(source)
+	cursor.execute(f'INSERT INTO {source} (id, time) VALUES (?, ?)', (chat_id,time))
+
 def set_group(chat_id,group) -> None | int:
 	"""
 	Adds chat id to DB to use timetable notifications /Добавляет ID чата в телеграмме для дальнейшего использования
@@ -32,6 +53,8 @@ def set_group(chat_id,group) -> None | int:
 	"""
 	cursor.execute('INSERT INTO Groups (id,groups) VALUES (?, ?)'
 	               'ON CONFLICT(id) DO UPDATE SET groups = excluded.groups', (chat_id,group))
+	for source in ("Weekly","Tomorrow","Today"):
+		add_to(source,chat_id,"08:00")
 	connection.commit()
 
 def is_in(chat_id,source) -> bool:
@@ -41,23 +64,30 @@ def is_in(chat_id,source) -> bool:
 	:param source: table of DB where it's need to search /Таблица БД где нужно искать
 	:return: True if chat in table, else False / True если чат есть в БД, иначе False
 	"""
-	if source not in ("Groups","Weekly","Tomorrow","Today"):
-		raise ValueError(f"Некорректное название таблицы {source} для поиска")
+	check_source(source)
 	cursor.execute(f"SELECT * FROM {source} WHERE id=?", (chat_id,))
 	return bool(cursor.fetchone())
 
-def search_in(chat_id, source) -> list:
+def return_from(chat_id, source) -> str:
 	"""
-	Returns list of data in table of DB/ Возвращает список данных в таблице БД
+	Returns time/group in table of DB / Возвращает группу/время из таблицы БД
 	:param chat_id: ID of telegram chat / ID чата в телеграмм
 	:param source: table of DB where it's need to search / Таблица БД где нужно искать
-	:return: List of data in table of DB / Список данных в таблице БД
+	:return: data in table of DB / Данные в таблице БД
 	"""
-	if source not in ("Groups","Weekly","Tomorrow","Today"):
-		raise ValueError(f"Некорректное название таблицы {source} для поиска")
+	check_source(source)
 	cursor.execute(f"SELECT * FROM {source} WHERE id=?", (chat_id,))
-	return cursor.fetchall()
+	return cursor.fetchall()[0]
 
+def return_table(source) -> list:
+	"""
+	Returns table of DB / Возвращает таблицу БД
+	:param source: table of DB where it's need to return / Таблица БД которую нужно вернуть
+	:return: table of DB / Таблица БД
+	"""
+	check_source(source)
+	cursor.execute(f"SELECT * FROM {source}")
+	return cursor.fetchall()
 
 def remove_from(chat_id,source) -> None:
 	"""
@@ -66,8 +96,7 @@ def remove_from(chat_id,source) -> None:
 	:param source: table of DB where it's need to search / Таблица БД где нужно искать
 	:return: None
 	"""
-	if source not in ("Groups","Weekly","Tomorrow","Today"):
-		raise ValueError(f"Некорректное название таблицы {source} для поиска")
+	check_source(source)
 	cursor.execute(f'DELETE FROM {source} WHERE id = ?', (chat_id,))
 	connection.commit()
 
--- timework.py
+++ timework.py
@@ -1,8 +1,12 @@
 import datetime
 import asyncio
 
+from database import return_from, return_table
+
 NOW=datetime.datetime.now()
 
 async def weekly() -> None:
 	if NOW.date().day!="Sunday":
-		await asyncio.sleep(3600*24)
+		await asyncio.sleep(60*60*24)
+	if NOW.time().hour not in return_table('Weekly'):
+		await asyncio.sleep(60*60)
