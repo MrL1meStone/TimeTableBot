b81efdae90587b3a72b328a45ff9eeceebf1bcf7
EnjiniaNoAjito <yannag10@mail.ru>
2025-11-07 22:13:08 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI, Request, Header
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from aiogram import Bot
 import uvicorn
 import json
-import asyncio
 import os
 
-SERVICE_ACCOUNT_FILE = "coral-firefly-472118-d4-0e2e192450f3.json"
-FOLDER_ID = '1jfQFPUpOuv_tNLyQIoipPVsLQHv_H5UQ'
+# SERVICE_ACCOUNT_FILE = "coral-firefly-472118-d4-0e2e192450f3.json"
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+FOLDER__GOOGLE_DRIVE_ID = os.environ.get("FOLDER_ID")
+SERVICE_ACCOUNT_FILE_DATA = os.environ.get("CREDENTIALS")
+SERVICE_ACCOUNT_FILE = json.loads(SERVICE_ACCOUNT_FILE_DATA)
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-service = build('drive', 'v3', credentials=credentials)
 TOKEN = os.environ.get("BOT_TOKEN")
-BOT_INSTANCE = Bot(token=TOKEN)
-TARGET_CHAT_ID = 'your_chat_id'
+bot = Bot(token=TOKEN)
+service = build('drive', 'v3', credentials=credentials)
+chat_id = 'your_chat_id'
 
 def process_notification(payload):
     try:
@@ -22,18 +24,17 @@ def process_notification(payload):
     except FileNotFoundError:
         data = {}
         results = service.files().list(
-        q=f"'{FOLDER_ID}' in parents and mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'", fields="files(id, name, modifiedTime)").execute()
+        q=f"'{FOLDER__GOOGLE_DRIVE_ID}' in parents and mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'", fields="files(id, name, modifiedTime)").execute()
         files = results.get('files', [])
         for file in files:
             data[file['id']] = {'name': file['name'], 'modifiedTime': file['modifiedTime']}
         with open('files.json', 'w', encoding='utf-8') as file:
             json.dump(data, file)
 
-    final_answer = ''
+    final_answer = None
     
     if payload.get('removed'):  #  Проверка на удаленный файл
          needed_file_id = payload["fileId"]
-         final_answer = f'Файл {data[needed_file_id]["name"]} был удален с диска'
          new_data = {}
          for ID in data.keys():
               if ID != needed_file_id:
@@ -67,13 +68,12 @@ async def handle_notification(
     print("Внимание! Получено уведомление")
 
     changes = process_notification(payload)
-    if len(changes) > len('Внимание! Кажется, '):
+    if changes:
          for chat in chat_id:
-              await bot.send_message(chat_id, f"Внимание! На диске изменение: {changes}")
+              await bot.send_message(chat, f"Внимание, расписание обновлено: {changes}")
 
     return {'status': 'ok'}
 
 if __name__ == '__main__':
     # Launching the server
     uvicorn.run('main:app', host='0.0.0.0', port=8000)  # Не финальный хост
-
